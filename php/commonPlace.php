9c2175a92378cc7090a8cac5a364d0bf9d5191d8
Casca <jscasca@gmail.com>
2015-09-12 13:49:52 -0400

    Add md5 hash to login query

--- php/commonPlace.php
+++ php/commonPlace.php
@@ -14,7 +14,7 @@ $queryUser =
 		users
 	WHERE
 		userName = '".$_POST['user']."' AND
-		userPass = '".$_POST['pass']."'";
+		userPass = '".md5($_POST['pass'])."'";
 $userList = mysql_query($queryUser,$conexion);
 $userCount = mysql_num_rows($userList);
 if($userCount==0)
